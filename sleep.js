fbac107e8b48a769d54035457da0571f037250a3
heishi1HUMANITY <i.wanna.make.this.main@gmail.com>
2020-01-11 11:20:07 +0900

    sleep sort

---
+++ sleep.js
@@ -0,0 +1,38 @@
+const randomArray = num => {
+    let array = [];
+    for(let i = 0; i < num; i++){
+        array.push(i);
+    }
+    for(let i = 0; i < num; i++){
+        let rand = Math.floor(Math.random() * (num - i));
+        let tmp = array[rand];
+        array[rand] = array[num - 1 - i];
+        array[num - 1 -i] = tmp;
+    }
+    return array;
+};
+
+const sleep = async array => {
+    let length = array.length;
+    let tmp = [];
+    for(let e of array){
+        setTimeout(() => {
+            tmp.push(e);
+        }, e * 2);
+    }
+    const sleepBreaker = () => {
+        return new Promise((resolve, reject) => {
+            const sleepBreakerInterval = setInterval(() => {
+                if(tmp.length == length){
+                    array = tmp;
+                    clearInterval(sleepBreakerInterval);
+                    resolve();
+                }
+            }, 1);
+        })
+    };
+    await sleepBreaker();
+    return array;
+}; 
+
+sleep(randomArray(100)).then(array => console.log(array));
